4dba067ed0b403844760ffb6bf7416be572fb94b
Skyler Ferrante <sjf5462@rit.edu>
2021-07-24 13:38:04 -0400

    Added current player, reset, and fixed some things

--- model/ConnectFour.java
+++ model/ConnectFour.java
@@ -3,19 +3,30 @@ package model;
 /**
  * ConnectFour model
  */
-public class ConnectFour {
+public class ConnectFour{
     public final int HEIGHT;
     public final int WIDTH;
 
     private Checker[][] board;
     private int[] numberOfMovesByRow;
 
+    private Checker currentPlayer;
+
     public ConnectFour(int height, int width){
         this.HEIGHT = height;
         this.WIDTH = width;
 
-        board = new Checker[HEIGHT][WIDTH];
+        board = new Checker[WIDTH][HEIGHT];
         numberOfMovesByRow = new int[WIDTH];
+
+        currentPlayer = Checker.RED;
+
+        reset();
+    }
+
+    public ConnectFour(){
+        //Default game has height 6, width 7
+        this(6, 7);
     }
 
     /**
@@ -25,7 +36,7 @@ public class ConnectFour {
      * @param y The y position of the checker
      * @return Checker value at board[x][y]
      */
-    public Checker getChecker(int x, int y) throws ConnectFourException{
+    public Checker getChecker(int x, int y){
         return board[x][y];
     }
 
@@ -35,16 +46,42 @@ public class ConnectFour {
      * @param value Value of checker to place
      * @throws ConnectFourException If the row is already full, or if the row is out of bounds
      */
-    public void placeChecker(int x, Checker value) throws ConnectFourException{
+    public void move(int x, Checker value) throws ConnectFourException{
+        //Check bounds
         if(x < 0 || x>=WIDTH){
             throw new ConnectFourException("Row "+x+" does not exist");
         }
+        //Check if row is full
         else if(numberOfMovesByRow[x] == HEIGHT){
             throw new ConnectFourException("Row "+x+" is already full!");
-        }else{
+        }
+        //Make move
+        else{
             int y = numberOfMovesByRow[x];
             board[x][y] = value;
-            numberOfMovesByRow[x] = y + 1;
+            numberOfMovesByRow[x] = y+1;
+        }
+        
+        //Switch current player
+        currentPlayer = (currentPlayer == Checker.RED) ? Checker.BLACK : Checker.RED;
+    }
+
+    /**
+     * Get the current players turn
+     * @return Current Players checker color
+     */
+    public Checker getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    /**
+     * Reset board to empty
+     */
+    public void reset(){
+        for(int x = 0; x < board.length; x++){
+            for(int y = 0; y < board[0].length; y++){
+                board[x][y] = Checker.EMPTY;
+            }
         }
     }
 }
