b401afe5a3f8ec9be2894fa6519fbb2e1011907b
Skyler Ferrante <sjf5462@rit.edu>
2021-07-24 13:37:00 -0400

    Added string representation to string enum

--- model/Checker.java
+++ model/Checker.java
@@ -1,7 +1,17 @@
 package model;
 
 public enum Checker {
-    EMPTY,
-    RED,
-    BLACK;
+    EMPTY(" "),
+    RED("R"),
+    BLACK("B");
+
+    private String representation;
+    private Checker(String representation){
+        this.representation = representation;
+    }
+
+    @Override
+    public String toString() {
+        return representation;
+    }
 }
