5b0727b8ae1dddb753de091e91dd414f5fd84464
Skyler Ferrante <sjf5462@rit.edu>
2021-07-24 20:17:32 -0400

    Cleaning up and added comments

--- view/ConnectFourGUI.java
+++ view/ConnectFourGUI.java
@@ -14,15 +14,18 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 
 public class ConnectFourGUI extends Application {
+    /**
+     * Homemade in MS-Paint, so not masterpieces
+     */
     private static final Image BLANK_IMAGE = new Image("images/blank.png");
     private static final Image BLACK_IMAGE = new Image("images/black.png");
-    private static final Image RED_IMAGE = new Image("images/red.png");
+    private static final Image RED_IMAGE   = new Image("images/red.png");
+    
     private static final Font FONT = new Font("Comic Sans MS", 30);
     private static final Effect BLACK_EFFECT = new InnerShadow(200, Color.BLACK);
     private static final Effect RED_EFFECT = new InnerShadow(200, Color.RED);
 
     private ConnectFour model;
-    
     private ImageView[][] images;
 
     @Override
@@ -56,10 +59,10 @@ public class ConnectFourGUI extends Application {
         for(int x = 0; x < model.WIDTH; x++){
             for(int y = 0; y < model.HEIGHT; y++){
                 Node node = makeConnectFourButton(x, y);
-                if(y == 0){
+                if(y == model.HEIGHT-1){
                     makeNodeClickable(node, x, status);
                 }
-                boardPane.add(node, x, y);
+                boardPane.add(node, x, model.HEIGHT-1-y);
             }
         }
 
@@ -68,7 +71,13 @@ public class ConnectFourGUI extends Application {
         stage.show();
     }
 
-
+    /**
+     * Creates buttons that will update their background based on the ConnectFour model
+     * 
+     * @param x X position of button to create
+     * @param y Y position of button to create
+     * @return Button that was created
+     */
     private Button makeConnectFourButton(int x, int y){
         ImageView squareView = new ImageView();
         squareView.setFitHeight(100);
@@ -80,21 +89,17 @@ public class ConnectFourGUI extends Application {
         updateImage(x, y, checker);
 
         Button button = new Button("", squareView);
-        button.setBackground(
-            new Background(
-                new BackgroundImage(
-                    BLANK_IMAGE,
-                    BackgroundRepeat.NO_REPEAT, 
-                    BackgroundRepeat.NO_REPEAT, 
-                    BackgroundPosition.CENTER,
-                    BackgroundSize.DEFAULT
-                )
-            )
-        );
         button.setPadding(new Insets(0));
         return button;
     }
 
+    /**
+     * Update image of checker that changed
+     * 
+     * @param x X position of checker
+     * @param y Y position of checker
+     * @param checker Checker value to set Checker at (x,y) to
+     */
     private void updateImage(int x, int y, Checker checker){
         ImageView squareView = images[x][y];
         switch(checker){
@@ -109,6 +114,13 @@ public class ConnectFourGUI extends Application {
         }
     }
 
+    /**
+     * Makes node clickable, and adds highlighted effect
+     * 
+     * @param button Node to make clickable
+     * @param x X position of Node
+     * @param status The status text box
+     */
     public void makeNodeClickable(Node button, int x, Text status){
         button.setOnMouseClicked((e) -> {
             try {
@@ -123,18 +135,22 @@ public class ConnectFourGUI extends Application {
 
         button.setOnMouseEntered((e) -> {
             Effect effect = (model.getCurrentPlayer() == Checker.RED) ? RED_EFFECT : BLACK_EFFECT;
-            images[x][0].setEffect(effect);
+            images[x][model.HEIGHT-1].setEffect(effect);
         });
         button.setOnMouseExited((e) -> {
-            images[x][0].setEffect(null);
+            images[x][model.HEIGHT-1].setEffect(null);
         });
     }
 
+    /**
+     * Set status text box to the current players name
+     * 
+     * @param status Status text box to change to player name
+     */
     public void setCurrentPlayer(Text status){
         Checker currentPlayer = model.getCurrentPlayer();
-        status.setText(
-            (currentPlayer == Checker.RED) ? "RED" : "BLACK"
-        );
+        status.setText(currentPlayer.name());
+
         status.setFill(
             (currentPlayer == Checker.RED) ? Color.RED : Color.BLACK
         );
